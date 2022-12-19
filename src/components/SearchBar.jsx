6eb0c38dd7920e2173fc74960af1a13860c64a1d
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-19 16:16:07 +0000

    SearchBar handleSubmit, handleChange

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,15 +2,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = ({ posts, setSearchResults }) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleChange = (e) => {
+        if (!e.target.value) return setSearchResults(posts);
+
+        const resultsArray = posts.filter(post =>
+            post.title.includes(e.target.value) ||
+            post.body.includes(e.target.value));
+
+        setSearchResults(resultsArray);
+    };
+
     return (
         <header>
-            <form className="d-flex my-3" role="search">
+            <form onSubmit={handleSubmit} className="d-flex my-3" role="search">
                 <input
                     className="form-control me-2"
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
                     id="search"
+                    onChange={handleChange}
                 />
                 <button
                     className="btn btn-outline-success"

e78de5361a6653232dc61906dfb3a7391964abf2
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-19 15:12:51 +0000

    Setting up Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import Contact from './pages/Contact';
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+
+      <main className='container my-3'>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route exact path="/posts" element={<Posts />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </>
   );
 }
 
